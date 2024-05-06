40983bc45451f4846072a7a59e5212251f3c7481
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2024-05-06 11:45:51 -0300

    feat: new route, connection db

--- src/server/main.go
+++ src/server/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"database/sql"
+	"log"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type User struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+var db *sql.DB
+
+func createTable() {
+	const query = `
+	CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		email VARCHAR(255) UNIQUE NOT NULL,
+		password VARCHAR(255) NOT NULL
+	);`
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatalf("Failed to create table: %v", err)
+	}
+	log.Println("Table created or verified successfully")
+}
+
+func initDB() {
+	var err error
+	db, err = sql.Open("postgres", "host=db port=5432 user=postgres password=admin123 dbname=postgres sslmode=disable")
+	if err != nil {
+		log.Fatalf("Failed to open database: %v", err)
+	}
+	if err = db.Ping(); err != nil {
+		log.Fatalf("Failed to connect to database: %v", err)
+	}
+
+	createTable() // Criar a tabela ao iniciar a conexão
+	log.Println("Connected to the database successfully")
+}
 
 func setupRouter() *gin.Engine {
 	r := gin.Default()
@@ -14,13 +54,36 @@ func setupRouter() *gin.Engine {
 
 	r.GET("/:name", func(c *gin.Context) {
 		name := c.Params.ByName("name")
-		c.String(http.StatusOK,"Hello, %s!", name)
+		c.String(http.StatusOK, "Hello, %s!", name)
+	})
+
+	r.POST("/users", func(c *gin.Context) {
+		var newUser User
+		if err := c.BindJSON(&newUser); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid data"})
+			return
+		}
+
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(newUser.Password), bcrypt.DefaultCost)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not hash password"})
+			return
+		}
+
+		_, err = db.Exec("INSERT INTO users (email, password) VALUES ($1, $2)", newUser.Email, string(hashedPassword))
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Could not save user"})
+			return
+		}
+
+		c.JSON(http.StatusCreated, gin.H{"status": "User created"})
 	})
 
 	return r
 }
 
 func main() {
+	initDB()
 	r := setupRouter()
 	r.Run(":8080")
 }
