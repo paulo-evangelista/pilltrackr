bd2e7d4121e80b22210b93353765a65a9028f0cb
MihaellK <mihaell.klosowski@gmail.com>
2024-05-22 01:35:18 -0300

    feat(flutter): modal open on list tile click

--- src/frontend/lib/widgets/list_tile_custom.dart
+++ src/frontend/lib/widgets/list_tile_custom.dart
@@ -11,6 +11,7 @@ class ListTileCustom extends StatefulWidget {
 }
 
 class _ListTileCustomState extends State<ListTileCustom> {
+  
   late String _title;
   late String _subtitle;
 
@@ -21,18 +22,61 @@ class _ListTileCustomState extends State<ListTileCustom> {
     _subtitle = widget.subtitle;
   }
 
+  void _showModal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Requsição #1234'),
+          content: Text('A requisição foi encaminhada para farmacia central \nStatus: Aguardando Envio'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Fechar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Chat'),
+              onPressed: () {
+                Navigator.of(context).pop(); // Fechar o modal
+                //Adicionar tela de chat para levar o user apos clicar  
+                //Navigator.push(
+                  //context
+                  //MaterialPageRoute(builder: (context) => SecondScreen()),
+                //);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('ListTile Sample')),
+      appBar: AppBar(
+        title: const Text('Requisições Feitas'),
+        centerTitle: true,
+        ),
       body: ListView(
         children: <Widget>[
           Card(
             child: ListTile(
-              leading: FlutterLogo(size: 72.0),
+              leading: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Icon(Icons.check_box_outlined)
+                ],
+              ),
               title: Text(_title),
               subtitle: Text(_subtitle),
-              trailing: Icon(Icons.more_vert),
+              trailing: Container(
+                height: double.infinity,
+                child: Icon(Icons.mark_chat_unread_outlined),
+              ),
+              onTap: () => _showModal(context),
               isThreeLine: true,
             ),
           ),
