f238a0cf85a6db1e8f9ae657d8c61f591dcafa87
amandafontes <amanda.fontes@sou.inteli.edu.br>
2024-05-23 02:00:59 -0300

    feat: adiciona botão para envio de requisições

--- src/frontend/lib/views/fc/requests.dart
+++ src/frontend/lib/views/fc/requests.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/request.dart';
 import '../../widgets/list_tile_custom.dart';
 
 class MyRequests extends StatelessWidget {
-  const MyRequests({super.key});
-
+  // ignore: use_key_in_widget_constructors
+  const MyRequests();
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +12,27 @@ class MyRequests extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Pilltrackr'),
       ),
-      body: const Center(
-        child: ListTileCustom(title: 'Requisição #1', subtitle: 'MS1371 - Dipirona 500mgR'),
-        // child: ElevatedButton(
-        //   onPressed: () => _showModal(context),
-        //   child: Text('Show Modal'),
-        // ),
-        
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              children: [
+                ListTileCustom(request: Request(id: '1', title: 'Requisição #1', status: 'MS1371 - Dipirona 500mg')),
+                ListTileCustom(request: Request(id: '2', title: 'Requisição #2', status: 'MS245 - Paracetamol 750mg')),
+                ListTileCustom(request: Request(id: '3', title: 'Requisição #3', status: 'MS789 - Amoxicilina 500mg')),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Lógica para enviar as requisições
+              },
+              child: const Text('Enviar'),
+            ),
+          ),
+        ],
       ),
     );
   }
--- src/frontend/lib/widgets/list_tile_custom.dart
+++ src/frontend/lib/widgets/list_tile_custom.dart
@@ -1,51 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/models/request.dart';
 
-class ListTileCustom extends StatefulWidget {
-  final String title;
-  final String subtitle;
+class ListTileCustom extends StatelessWidget {
+  final Request request;
 
-  const ListTileCustom({super.key, required this.title, required this.subtitle});
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _ListTileCustomState createState() => _ListTileCustomState();
-}
-
-class _ListTileCustomState extends State<ListTileCustom> {
-  
-  late String _title;
-  late String _subtitle;
-
-  @override
-  void initState() {
-    super.initState();
-    _title = widget.title;
-    _subtitle = widget.subtitle;
-  }
+  const ListTileCustom({super.key, required this.request});
 
   void _showModal(BuildContext context) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text('Requsição #1234'),
-          content: const Text('A requisição foi encaminhada para a farmácia central \nStatus: Aguardando envio'),
+          title: Text('Requisição ${request.id}'),
+          content: Text('A requisição foi encaminhada para a farmácia central \nStatus: ${request.status}'),
           actions: <Widget>[
             TextButton(
               child: const Text('Fechar'),
               onPressed: () {
                 Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('Chat'),
-              onPressed: () {
-                Navigator.of(context).pop(); // Fechar o modal
-                //Adicionar tela de chat para levar o user apos clicar  
-                //Navigator.push(
-                  //context
-                  //MaterialPageRoute(builder: (context) => SecondScreen()),
-                //);
               },
             ),
           ],
@@ -56,33 +28,13 @@ class _ListTileCustomState extends State<ListTileCustom> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Requisições Feitas'),
-        centerTitle: true,
-        ),
-      body: ListView(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-              leading: const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.check_box_outlined)
-                ],
-              ),
-              title: Text(_title),
-              subtitle: Text(_subtitle),
-              trailing: const SizedBox(
-                height: double.infinity,
-                child: Icon(Icons.mark_chat_unread_outlined),
-              ),
-              onTap: () => _showModal(context),
-              isThreeLine: true,
-            ),
-          ),
-        ],
-      ),
+    return ListTile(
+      leading: const Icon(Icons.check_box_outlined),
+      title: Text(request.title),
+      subtitle: Text(request.status),
+      trailing: const Icon(Icons.mark_chat_unread_outlined),
+      onTap: () => _showModal(context),
+      isThreeLine: true,
     );
   }
 }
