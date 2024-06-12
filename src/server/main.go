883ced66db6d600f4521e145c8f3e095b852586d
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-06-11 23:10:01 -0300

    refactor(server): ♻️ moved prometheus setup

--- src/server/main.go
+++ src/server/main.go
@@ -6,31 +6,13 @@ import (
 	"g5/server/middlewares"
 	"g5/server/types"
 	"github.com/gin-gonic/gin"
-	"github.com/penglongli/gin-metrics/ginmetrics"
 )
 
 func main() {
 
 	r := gin.Default()
 
-	m := ginmetrics.GetMonitor()
-	
-	m.AddMetric(&ginmetrics.Metric{
-		Type: 	ginmetrics.Counter,
-		Name: "db_requests_operations_total",
-		Description: "Total number of db requests operations",
-		Labels:      []string{"label1"},
-	})
-
-	m.AddMetric(&ginmetrics.Metric{
-		Type: 	ginmetrics.Counter,
-		Name: "db_users_operations_total",
-		Description: "Total number of db user operations",
-		Labels:      []string{"label1"},
-	})
-
-	m.SetMetricPath("/metrics")
-	m.Use(r)
+	db.SetupPrometheus(r)
 
 	clients := types.Clients{
 		Redis: db.SetupRedis(),
@@ -41,7 +23,6 @@ func main() {
 	r.Use(middlewares.AuthUser())
 	r.Use(middlewares.AssertUserExistance(clients.Pg))
 
-	// controllers.InitReportRoutes(r, clients)
 	controllers.InitRequestRoutes(r, clients)
 	controllers.InitClientRoutes(r, clients)
 	controllers.InitAdminRoutes(r, clients)
