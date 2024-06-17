9827bb74bb4e3bcbe9d3a74a221c1004ca919182
VZeferino <vitor.queiroz@sou.inteli.edu.br>
2024-06-17 11:05:40 -0300

    chat finished

--- src/frontend/lib/views/chat/chat.dart
+++ src/frontend/lib/views/chat/chat.dart
@@ -1,20 +1,15 @@
 import 'dart:convert';
-import 'dart:io';
 
-import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:http/http.dart' as http;
-import 'package:intl/date_symbol_data_local.dart';
-import 'package:mime/mime.dart';
-import 'package:open_filex/open_filex.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:uuid/uuid.dart';
+import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
 void main() {
-  initializeDateFormatting().then((_) => runApp(const MyApp()));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -55,12 +50,12 @@ class _ChatPageState extends State<ChatPage> {
   }
 
   void _connectWebSocket() {
-    final token = 'admin';
+    final token = "paulo";
     final url = 'wss://pilltrackr.cathena.io/ws/?token=$token';
 
     try {
-      _channel = WebSocketChannel.connect(Uri.parse(url));
-      
+      _channel = IOWebSocketChannel.connect(Uri.parse(url));
+
       _channel.stream.listen((message) {
         final decodedMessage = json.decode(message);
         _handleIncomingMessage(decodedMessage);
@@ -82,7 +77,7 @@ class _ChatPageState extends State<ChatPage> {
   void _handleIncomingMessage(Map<String, dynamic> message) {
     if (message['request'] == request) {
       final newMessage = types.TextMessage(
-        author: types.User(id: message['from']), // Assumindo que 'from' é o ID do autor
+        author: types.User(id: message['from']),
         createdAt: DateTime.now().millisecondsSinceEpoch,
         id: const Uuid().v4(),
         text: message['content'],
@@ -94,106 +89,8 @@ class _ChatPageState extends State<ChatPage> {
 
   void _addMessage(types.Message message, {bool incoming = false}) {
     setState(() {
-      _messages.insert(0, message);
+      _messages.add(message);
     });
-  }
-
-  void _handleAttachmentPressed() {
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) => SafeArea(
-        child: SizedBox(
-          height: 144,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  _handleFileSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('File'),
-                ),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text('Cancel'),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  void _handleFileSelection() async {
-    final result = await FilePicker.platform.pickFiles(
-      type: FileType.any,
-    );
-
-    if (result != null && result.files.single.path != null) {
-      final message = types.FileMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-        mimeType: lookupMimeType(result.files.single.path!),
-        name: result.files.single.name,
-        size: result.files.single.size,
-        uri: result.files.single.path!,
-      );
-
-      _addMessage(message);
-    }
-  }
-
-  void _handleMessageTap(BuildContext _, types.Message message) async {
-    if (message is types.FileMessage) {
-      var localPath = message.uri;
-
-      if (message.uri.startsWith('http')) {
-        try {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: true,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-
-          final client = http.Client();
-          final request = await client.get(Uri.parse(message.uri));
-          final bytes = request.bodyBytes;
-          final documentsDir = (await getApplicationDocumentsDirectory()).path;
-          localPath = '$documentsDir/${message.name}';
-
-          if (!File(localPath).existsSync()) {
-            final file = File(localPath);
-            await file.writeAsBytes(bytes);
-          }
-        } finally {
-          final index =
-              _messages.indexWhere((element) => element.id == message.id);
-          final updatedMessage =
-              (_messages[index] as types.FileMessage).copyWith(
-            isLoading: null,
-          );
-
-          setState(() {
-            _messages[index] = updatedMessage;
-          });
-        }
-      }
-
-      await OpenFilex.open(localPath);
-    }
   }
 
   void _handleSendPressed(types.PartialText message) {
@@ -227,15 +124,15 @@ class _ChatPageState extends State<ChatPage> {
         final isSentByUser = json['SentByUser'] ?? false;
 
         return types.TextMessage(
-          author: isSentByUser ? _user : _admin, // Define o autor com base em SentByUser
+          author: isSentByUser ? _user : _admin,
           createdAt: DateTime.parse(json['CreatedAt']).millisecondsSinceEpoch,
-          id: json['ID'].toString(), // Convertendo ID para string
+          id: json['ID'].toString(),
           text: json['Content'],
         );
       }).toList();
 
       setState(() {
-        _messages = messages;
+        _messages = messages.reversed.toList(); // Inverter a lista de mensagens
       });
     } else {
       print('Failed to load messages');
@@ -244,14 +141,19 @@ class _ChatPageState extends State<ChatPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Chat'),
+        ),
         body: Chat(
           messages: _messages,
-          onAttachmentPressed: _handleAttachmentPressed,
-          onMessageTap: _handleMessageTap,
           onSendPressed: _handleSendPressed,
           showUserAvatars: true,
           showUserNames: true,
           user: _user,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _loadMessages,
+          child: const Icon(Icons.refresh),
         ),
       );
 }
