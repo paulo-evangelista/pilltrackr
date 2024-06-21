ae9d1900ff8904a56f78bfb870ce8d4cbf514ed9
MihaellK <mihaell.klosowski@gmail.com>
2024-06-20 21:01:35 -0300

    show debug label set off

--- src/frontend/lib/main.dart
+++ src/frontend/lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pilltrackr',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(40, 57, 104, 1)),
         useMaterial3: true,
