d2f8bc4c4d464b8381105bd9ed891a070d1b1606
paulo-evangelista <paulo.evangelista@sou.inteli.edu.br>
2024-07-03 01:55:32 -0300

    removed folders

--- tests/locust/user.py
+++
@@ -1,8 +0,0 @@
-from locust import HttpUser, task, between
-
-class WebsiteUser(HttpUser):
-    wait_time = between(1, 5)
-
-    @task
-    def criar_usuario(self):
-        self.client.post("/users", json={"email": "vitor@gmail.com",  "password": "123@vitor" })
